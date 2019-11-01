110a3a034ea0451da04c6fe92b2a3600687a6b01
Steven Lin <slin@merchantgrowth.com>
2019-11-01 16:55:20 -0700

    create Heroes feature

---
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from "@angular/core";
+import { Observable } from 'rxjs';
+import { Hero } from '../hero';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+    selector: 'app-hero-detail',
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+    hero$: Observable<Hero>;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private service: HeroService
+    ){}
+    
+    ngOnInit() {
+        this.hero$ = this.route.paramMap.pipe(
+            switchMap(
+                (params: ParamMap) =>
+                    this.service.getHero(params.get('id'))
+            )
+        );          
+    }
+
+    gotoHeroes(hero: Hero) {
+        let heroId = hero ? hero.id : null;
+        // Pass along the hero id if available
+        // so that the HeroList component can select that hero.
+        // Include a junk 'foo' property for fun.
+        this.router.navigate(['/superheroes', { id: heroId, foo: 'foo'}]);
+    }
+
+
+
+}
